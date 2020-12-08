244aafbaeb6bdd64992af0e162435537cce8ad87
ChrisLee <ChengDongli997@163.com>
2020-12-08 12:20:26 +0800

    database-manage

--- esPlugsConfig.php
+++ esPlugsConfig.php
@@ -3,7 +3,7 @@
 return [
     'name'      => '数据库备份迁移管理',
     'des'       => '数据库备份迁移管理插件',
-    'namespace' => "Qxplugs\\DatabaseManage\\",
+    'namespace' => "Chrisplugs\\DatabaseManage\\",
     'version'   => '1.0',
     'logo'      => ""
 ];
