5a840f341d84c02aabe60164b17252849af80fce
李成东 <ChengDongli997@163.com>
2020-12-08 11:46:59 +0800

    Add files via upload

---
+++ esPlugsConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name'      => '数据库备份迁移管理',
+    'des'       => '数据库备份迁移管理插件',
+    'namespace' => "Qxplugs\\DatabaseManage\\",
+    'version'   => '1.0',
+    'logo'      => ""
+];
