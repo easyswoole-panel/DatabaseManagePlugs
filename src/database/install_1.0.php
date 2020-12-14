8d3c2441e64b8f5520ba947264c09a4ecdc348e2
ChirsLeeAreemm <ChengDongli997@163.com>
2020-12-14 15:38:58 +0800

    更新:使用基础包助手类 Update:Use BasePlugsHelper
    Signed-off-by: ChirsLeeAreemm <ChengDongli997@163.com>

--- src/database/install_1.0.php
+++ src/database/install_1.0.php
@@ -10,7 +10,7 @@
 namespace Chrisplugs\DatabaseManage\database;
 use Chrisplugs\DatabaseManage\common\Helper;
 use EasySwoole\EasySwoole\Config;
-use Siam\Plugs\common\PlugsHelper;
+use Siam\Plugs\common\PlugsTableHelper;
 use EasySwoole\DDL\Blueprint\Table;
 use EasySwoole\DDL\DDLBuilder;
 use EasySwoole\DDL\Enum\Character;
@@ -25,47 +25,14 @@ use EasySwoole\ORM\DbManager;
  */
 class Install
 {
-
-    /**
-     * 日志表
-     * @var string database_backup_logs
-     */
-    private $logTable;
-
-
-
     /**
      * 安装流程
      * @return bool
      */
     public function install()
     {
-        //检查前缀
-        $prefix = Config::getInstance()->getConf('MYSQL.prefix');
-        //设定表名
-        $this->logTable  = "{$prefix}database_backup_logs";
-        //助手类
-        $Helper = Helper::getInstance();
-        //检查是否存在表，不存在则创建,使用默认连接
-        $checkTable = $Helper->checkTable($this->logTable);
-            if (!$checkTable){
-                $create = $this->createLogTable();
-                if (!$create){
-                    return false;
-                }
-            }
-        return true;
-    }
-
-    /**
-     * 创建日志表
-     * @param string $connection 指定的数据库连接
-     * @return mixed
-     */
-    protected function createLogTable($connection='default')
-    {
-        //创建表DDL
-        $sql = DDLBuilder::table("{$this->logTable}",function (Table $table){
+        //使用基础包提供的助手类建表
+        return PlugsTableHelper::getInstance()->create('database_backup_logs',function (Table $table){
             $table->setTableComment('数据库备份记录表')
                 ->setTableEngine(Engine::MYISAM)
                 ->setTableCharset(Character::UTF8MB4_GENERAL_CI);
@@ -73,11 +40,6 @@ class Install
             $table->colVarChar('name',50)->setColumnComment('备份文件名')->setIsNotNull();
             $table->colInt('backup_at',10)->setIsNotNull()->setColumnComment('创建时间');
         });
-
-        $queryBuild = new QueryBuilder();
-        $queryBuild->raw($sql);
-        return DbManager::getInstance()->query($queryBuild, true, $connection)->getResult();
-
     }
 }
 //go install
