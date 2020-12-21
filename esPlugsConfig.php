f0f5de1afcc786c94d5b7f01fdefb4fe47047158
ChrisLee <ChengDongli997@163.com>
2020-12-21 15:06:14 +0800

    logo
    Signed-off-by: ChrisLee <ChengDongli997@163.com>

--- esPlugsConfig.php
+++ esPlugsConfig.php
@@ -5,6 +5,6 @@ return [
     'des'       => '数据库备份迁移管理插件',
     'namespace' => "Chrisplugs\\DatabaseManage\\",
     'version'   => '1.0',
-    'logo'      => "",
+    'logo'      => "https://www.chrisleesir.com/wp-content/uploads/2020/12/timg.jpeg",
     'home_view' => ''
 ];
