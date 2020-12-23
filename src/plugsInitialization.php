06b99f4658df640c09833a63832e6140827a0b6b
ChrisLee <ChengDongli997@163.com>
2020-12-23 16:50:30 +0800

    完善版本V1 备份、历史记录
    Signed-off-by: ChrisLee <ChengDongli997@163.com>

--- src/plugsInitialization.php
+++ src/plugsInitialization.php
@@ -5,3 +5,25 @@
  * Date: 2020/12/1 0001
  * Time: 22:55
  */
+namespace Chrisplugs\DatabaseManage;
+use Chrisplugs\DatabaseManage\controller\BackupController;
+use Siam\Plugs\common\PlugsRouterHelper;
+
+
+class PlugsInitialization
+{
+    /**
+     * 初始化 提供插件管理的api
+     * @api /api/chris_plugs/db_manage/get_logs
+     * @api /api/chris_plugs/db_manage/back_up
+     */
+    public static function initPlugsRouter()
+    {
+        PlugsRouterHelper::getInstance()->addAnyRouter([
+            '/api/chris_plugs/db_manage/get_logs' => [new BackupController, 'get_logs'],
+            '/api/chris_plugs/db_manage/back_up' => [new BackupController, 'back_up'],
+        ]);
+
+    }
+}
+PlugsInitialization::initPlugsRouter();
